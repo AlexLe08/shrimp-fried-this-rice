fbc3b51e5b31cabd7d63a51992f3e3af0b775c33
AlexLe08 <8702466+AlexLe08@users.noreply.github.com>
2026-08-28 17:53:34 -0400

    feat: implement command handling and add utility commands (ping, server, user)

---
+++ src/commands/utility/server.ts
@@ -0,0 +1,17 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+
+export default {
+	data: new SlashCommandBuilder().setName('server').setDescription('Provides information about the server.'),
+	async execute(interaction: ChatInputCommandInteraction) {
+		// interaction.guild is the object representing the Guild in which the command was run
+
+        if (!interaction.inCachedGuild()) {
+            await interaction.reply("This command can only be used in a server.");
+            return;
+        }
+
+		await interaction.reply(
+			`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`,
+		);
+	},
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
-import 'dotenv/config'
+import { Client, Collection, Events, GatewayIntentBits, MessageFlags } from "discord.js";
+import 'dotenv/config';
+
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -7,5 +14,59 @@ client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+// reads the path to the directory and returns an array of all the folder names it contains
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	// reads the path to this directory and returns an array of all the file names they contain,
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = await import (filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		// For each file being loaded, check that it has at least the data and execute properties.
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	// Check if the interaction is a chat input command
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	// If the command doesn't exist, log an error and return
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	// Try to execute the command and catch any errors that occur during execution
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		} else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	}
+
+	console.log(interaction);
+});
 
 client.login(process.env.DISCORD_TOKEN);
